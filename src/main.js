9cdebd3c6ec614927b1f7336541aacbfaf93e353
Jiawei <liujiaweidreamer@gmail.com>
2021-08-16 18:15:20 +0800

    a demo start with native JavaScript

--- src/main.js
+++ src/main.js
@@ -1,3 +1,21 @@
 console.log('hi');
 console.log(React);
 console.log(ReactDOM);
+
+const $textarea = document.querySelector('#textarea');
+const $addBtn = document.querySelector('#add');
+const $minusBtn = document.querySelector('#minus');
+
+let number = 0;
+
+$textarea.innerText = "" + number;
+
+$addBtn.addEventListener('click', ()=>{
+  number += 1;
+  $textarea.innerText = "" + number;
+})
+
+$minusBtn.addEventListener('click', ()=>{
+  number -= 1;
+  $textarea.innerText = "" + number;
+})
