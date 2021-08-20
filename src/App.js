8de97af712067893cdd9f0743345c5e3f0ea7e52
Jiawei <liujiaweidreamer@gmail.com>
2021-08-20 15:26:29 +0800

    complete Counter with React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      number: 0
+    }
+  }
+  add1() {
+    this.setState({
+      number: this.state.number + 1
+    })
+  }
+  minus1() {
+    this.setState({
+      number: this.state.number - 1
+    })
+  }
+  render() {
+    return (
+      <div className="App">
+        the number {this.state.number} will change if you click
+        <button onClick={this.add1.bind(this)}>+1</button>
+        <button onClick={()=>{this.minus1()}}>-1</button>
+      </div>
+    );
+  }
 }
 
 export default App;
